800e12087c77e8e0c4f208ee194ab4889c6a1c14
elijah <1198331919@qq.com>
2019-06-11 17:26:49 +0800

    feat(service): recode the cart & order logic in service mode

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -3,15 +3,25 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\AddCartRequest;
-use App\Models\CartItem;
+// use App\Models\CartItem;
 use App\Models\ProductSku;
+use App\Services\CartService;
 use Illuminate\Http\Request;
 
 class CartController extends Controller
 {
+    protected $cartService;
+
+    // 利用 Laravel 的自动解析功能注入 CartService 类
+    public function __construct(CartService $cartService)
+    {
+        $this->cartService = $cartService;
+    }
+
     public function index(Request $request)
     {
-        $cartItems = $request->user()->cartItems()->with(['productSku.product'])->get();
+        // $cartItems = $request->user()->cartItems()->with(['productSku.product'])->get();
+        $cartItems = $this->cartService->get();
         $addresses = $request->user()->addresses()->orderBy('last_used_at', 'desc')->get();
 
         return view('cart.index', ['cartItems' => $cartItems, 'addresses' => $addresses]);
@@ -19,7 +29,7 @@ class CartController extends Controller
 
     public function add(AddCartRequest $request)
     {
-        $user = $request->user();
+        /*$user = $request->user();
         $skuId = $request->input('sku_id');
         $amount = $request->input('amount');
 
@@ -35,14 +45,16 @@ class CartController extends Controller
             $cart->user()->associate($user);
             $cart->productSku()->associate($skuId);
             $cart->save();
-        }
+        }*/
+        $this->cartService->add($request->input('sku_id'), $request->input('amount'));
 
         return [];
     }
 
     public function remove(Request $request, ProductSku $sku)
     {
-        $request->user()->cartItems()->where('product_sku_id', $sku->id)->delete();
+        // $request->user()->cartItems()->where('product_sku_id', $sku->id)->delete();
+        $this->cartService->remove($sku->id);
 
         return [];
     }
